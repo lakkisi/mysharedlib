07642027475a7a40698c5606fe142553522873f4
LAKKIS Ibrahim <Ibrahim.LAKKIS@ag2rlamondiale.fr>
2022-01-29 16:30:08 +0100

    first version

---
+++ src/com/silka/mysharedlib/MySharedlibWithUT.groovy
@@ -0,0 +1,10 @@
+package com.silka.mysharedlib ;
+
+public class MySharedlibWithUT implements Serializable   {
+
+
+	checkVersionFromPom (steps) {
+		if (steps.fileExists("pom.xml"))
+		def version = 
+	}
+}
