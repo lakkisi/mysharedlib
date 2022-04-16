922a9bd519d89c74125834bbf4be841ec9c60be7
LAKKIS Ibrahim <ibrahim.lakkis@ag2rlamondiale.fr>
2022-04-16 15:41:34 +0200

    fix import com.silka.mysharedlib.MySharedlibWithUT

--- vars/getVersionFromPom.groovy
+++ vars/getVersionFromPom.groovy
@@ -1,4 +1,5 @@
-import com.silka.sharedlib
+#!/usr/bin/groovy
+import com.silka.mysharedlib.MySharedlibWithUT
 def call() {
     def mysharedlib = new MySharedlibWithUT(this) // pass this to the object
     echo "version is :"+mysharedlib.getVersionFromPom()
